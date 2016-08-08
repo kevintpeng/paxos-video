851d852addccfc19b7edb2974f49b77ad26a23ea
Kevin Peng <kevin.peng@shopify.com>
2016-08-07 22:23:10 -0400

    altered algorithm implementation for more correctness

--- js/paxos.js
+++ js/paxos.js
@@ -1,118 +1,121 @@
 'use strict';
 const TIMEOUT = 3000;
+// NOTES
+// could write function majority which checks current list of connections
+// this would require responses to hold node ids
 
 class Paxos {
   constructor(options) {
     this.id = options.id;
-    this.comms = options.comms;
-    this.state = null;
-    this.seq = null;
-    this.current_proposal = null;
-    this.accepted_seq = null;
+    //this.comms = options.comms;
+    this.correct_val = null;
+    this.promised_val = null; // {seq => {default: {}, responses: []} }
+    this.active_proposals = [];
+    this.last_seq = null;
 
     // Listen for events from peers
     this.comms.peer.on('connection', this.receiveEvent);
   }
 
-  sendProposal() {
-    if ((new Date).getTime() - this.current_proposal.start_time > TIMEOUT) {
-      this.current_proposal = null;
-    }
-
-    if (!this.state || this.current_proposal){
-      return;
-    }
-
-    this.current_proposal = {
-      start_time: (new Date).getTime(),
-      total: this.comms.peers.length,
-      promise: [],
-      reject: [],
-      response_tuples: [{ state: this.state, seq: this.seq }]
-    };
-
-    this.seq = {
-      time: this.current_proposal['start_time'],
-      id: this.id
-    }; // tuple, for absolute uniqueness
-
-    var peers = this.comms.peers;
-    for(var peerIdx in peers) {
-      var peer = peers[peerIdx];
-      this.comms.sendDataToPeer(peer, 'PROPOSE', { state: this.state, seq: this.seq });
-    }
+  newProposal() {
+    // state = get state of the current video
+    seq = {time: (new Date).getTime(), id: this.id}
+    return {seq: seq, val: state}
   }
 
-  receiveEvent(connection) {
-    connection.on('data', function(event){
-      var id = connection.label;
-      console.log('Receiving event from ' + id);
+  sendPrepare() {
+    proposal = newProposal()
+    sendToAll('PREPARE', proposal)
+  }
 
-      switch (event.type) {
-      case 'PROPOSE':
-        this.receiveProposal(id, event.data);
-        break;
-      case 'RESPONSE':
-        this.receiveResponse(id, event.data);
-        break;
-      case 'COMMIT':
-        this.receiveCommit(id, event.data);
-        break;
-      default:
-        console.error('Received an unknown event (' + event.type + ') from peer ' + id);
-        break;
+  receivePrepare(proposer, proposal) {
+    if(Paxos.sequenceGt(proposal.seq, this.last_seq)) {
+      sendResponse(proposer, proposal.seq.time, {seq: this.last_seq, val: this.promised_val})
+      if(this.promised_val == null){
+        this.promised_val = proposal.val
+        this.last_seq = proposal.seq
       }
-    });
-  }
-
-  receiveProposal(id, proposal) {
-    if (proposal.seq > this.seq) {
-      this.comms.sendDataToPeer(id, 'RESPONSE', { accept: true, state: this.state, seq: proposal.seq }); // undefined state & seq if it hasn't accepted anything yet
-      this.accepted_seq = proposal.seq;
-    } else {
-      this.comms.sendDataToPeer(id, 'RESPONSE', { accept: false, state: this.state, seq: this.seq }); // this is important because this is how newest states are communicated to old proposals
     }
   }
 
-  receiveResponse(id, response) {
-    if (!this.current_proposal || (new Date).getTime() - this.current_proposal.start_time > TIMEOUT) {
-      // end current proposal
-      this.current_proposal = null;
-      return;
-    }
+  sendResponse(proposer, proposal_key, promised_proposal){
+    sendToNode(proposer, 'RESPONSE', proposal_key, promised_proposal)
+  }
 
-    if (response.accept) {
-      this.current_proposal.promise.push(id);
-    } else {
-      this.current_proposal.reject.push(id);
-    }
-    this.current_proposal.response_tuples.push(response);
-
-    var promiseCount = this.current_proposal.promise.length;
-    var rejectionCount = this.current_proposal.reject.length;
-
-    if (promiseCount / this.current_proposal.total > 0.5) {
-      // TODO: need to write edge case
-      var times = this.current_proposal.response_tuples.map(function(o){ return o.seq.time; });
-      var highest_sequence = Math.max.apply(Math, times);
-      var peers = this.comms.peers;
-      for (var peerIdx in peers) {
-        var peer = peers[peerIdx];
-        this.comms.sendDataToPeer(peer, 'COMMIT', highest_sequence);
+  receiveResponse(proposal_key, promised_proposal) {
+    if(this.current_proposals[proposal_key]){
+      this.current_proposals[proposal_key].responses.push(promised_proposal)
+      if(majority(this.current_proposals)) {
+        sendAccept(proposal_key)
       }
-    } else if (promiseCount + rejectionCount == this.current_proposal.total) {
-      // We've received all responses, but they did not reach majority, reset proposal
-      this.current_proposal = null;
     }
   }
 
-  receiveCommit(id, proposal) {
-    if (proposal.seq > this.seq && proposal.seq >= this.accepted_seq) {
-      this.state = proposal.state;
-      this.seq = proposal.seq;
-      this.accepted_seq = null;
+  // the key for current proposals is the time
+  sendAccept(key) {
+    proposal = maxProposal(this.current_proposals[key].responses)
+    if(proposal == null) {
+      proposal = this.current_proposals[key].default
+    }
+    proposal.seq = this.current_proposals[key].default.seq
+    this.current_proposals.delete(key)
+    sendToAll('ACCEPT', proposal)
+  }
+
+  receiveAccept(proposal) {
+    if(Paxos.sequenceGt(proposal.seq, this.last_seq)) {
+      // COMMIT
+      this.correct_val = proposal.val
+      this.promised_val = null
+      this.last_seq = proposal.seq
+      }
     }
   }
-}
 
-module.exports = Paxos;
+  Paxos.sequenceGt(seq1, seq2) {
+    if(seq1.time > seq2.time) {
+      return true
+    } else if (seq1.time == seq2.time && seq1.id > seq2.id) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  Paxos.maxProposal(proposals) {
+    max = null
+    for proposal in proposals {
+      if(max == null) {
+        // the final result will be null if all proposals are null
+        proposal = null
+      } else if Paxos.sequenceGt(proposal.seq, max.seq){
+        max = proposal
+      }
+    }
+    return max
+  }
+
+  Paxos.sendToAll(id, type, *stuff) {
+
+  }
+
+  Paxos.sendToNode(id, type, *stuff) {
+
+  }
+
+  receiveEvent(type, args) {
+    switch (type) {
+    case 'PREPARE':
+      this.receivePrepare(args[0], args[1]);
+      break;
+    case 'RESPONSE':
+      this.receiveResponse(args[0], args[1]);
+      break;
+    case 'COMMIT':
+      this.receiveAccept(args[0]);
+      break;
+    default:
+      console.error('Received an unknown event (' + type + ') from peer ' + id); // TODO no id defined
+      break;
+    }
+  }
